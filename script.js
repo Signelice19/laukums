05e15510229980c4efa503189dc953d0a788ed15
Signelice19 <avotins71@gmail.com>
2022-02-28 15:19:54 +0200

    2

--- script.js
+++ script.js
@@ -17,22 +17,61 @@ function perimetrs(m1, m2, m3) {
     m1 = m.m1; m2 = m.m2; m3 = m.m3
     console.log(m1, m2, m3);
     const perimetrs = m1 + m2 + m3;
-    console.log("perimetrs "+perimetrs);
+    console.log("perimetrs " + perimetrs);
     return (perimetrs);
 }
 
-function pusperimetrs(m1,m2,m3) {
-    const pusperimetrs = perimetrs(m1, m2, m3)/2
-    console.log("pusperimetrs "+pusperimetrs);
-    return(pusperimetrs);
+function pusperimetrs(m1, m2, m3) {
+    const pusperimetrs = perimetrs(m1, m2, m3) / 2
+    console.log("pusperimetrs " + pusperimetrs);
+    return (pusperimetrs);
 }
 
-function laukums(m1,m2,m3){
-    const pusperimetrs= perimetrs(m1,m2,m3)/2;
-    let tlaukums=Math.sqrt(
-        pusperimetrs*((pusperimetrs-m1)*(pusperimetrs-m2)*(pusperimetrs-m3)));
-        console.log(tlaukums);
-        return tlaukums;
-    }
+function laukums(m1, m2, m3) {
+    const pusperimetrs = perimetrs(m1, m2, m3) / 2;
+    let tlaukums = Math.sqrt(
+        pusperimetrs * ((pusperimetrs - m1) * (pusperimetrs - m2) * (pusperimetrs - m3)));
+    console.log(tlaukums);
+    return tlaukums;
+}
 
+function irTrijsturis(m1, m2, m3) {
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function rezultats(){
+    const m=nolasa();
+    m1=m.m1;m2=m.m2;m3=m.m3;
+    console.log(m1,m2,m3);
+    if(!nolasa()){
+        t="Nekorekti ievaddati, trijstūra malu vērtībām ir jābūt lielākam par 0";
+    }else{
+    if(irTrijsturis(m1,m2,m3)==false){
+        t="Trijstūris neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
+    }else{
+        t="Trijstūris ar malu garumiem"+m1+","+m2+" un "+m3+"eksistē".
+        if(m1==m2 && m2==m3){
+            t+="Tas ir vienādmalu trijstūris."
+        }
+        const p=perimetrs(m1,m2,m3);
+        const s=Math.round(laukums(m1,m2,m3)*100)/100;
+        t+="Perimetrs ir"+p+"un laukums ir"+s+".";
+    }
+}
+console.log(t);
+return t;
+}
+
+function izvadaTekstu(){
+    const teksts=rezultats();
+    console.log(teksts);
+    const sakne=document.getElementByld("izvade");
+    const raksti=document.createElement("p");
+    sakne.innerHTML=teksts;
+    sakne.appendChild(raksti);
+}
 
